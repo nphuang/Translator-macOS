7dac17d3933e4539c30899f29a7bffc25e494930
PHH <phqq.cs10@nycu.edu.tw>
2024-03-31 23:21:36 +0800

    Initial Commit

---
+++ macTranslator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macTranslator
+//
+//  Created by 黃鵬軒 on 2024/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macTranslator/macTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  macTranslatorApp.swift
+//  macTranslator
+//
+//  Created by 黃鵬軒 on 2024/3/31.
+//
+
+import SwiftUI
+
+@main
+struct macTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
